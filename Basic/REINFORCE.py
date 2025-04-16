1ead1d03d733c6ced30cfd602088efa9f463bc68
Liangyawei Kuang <33073967+klyw1998@users.noreply.github.com>
2025-04-16 08:48:57 +0800

    Add files via upload

--- Basic/REINFORCE.py
+++ Basic/REINFORCE.py
@@ -1,118 +1,119 @@
-# -*- coding: utf-8 -*-
-"""
-@coder: Liangyawei Kuang
-
-OS-Windows 10
-
-Python-3.9.7
-Gym-0.21.0
-PyTorch-1.10.2
-"""
-
-import argparse
-import gym
 import numpy as np
-from itertools import count
-
+import gymnasium as gym
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torch.distributions import Categorical
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import rl_utils
 
+# Check whether GPU is available
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print(device)
 
-parser = argparse.ArgumentParser(description='REINFORCE')
-parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
-                    help='discount factor (default: 0.99)')
-parser.add_argument('--seed', type=int, default=543, metavar='N',
-                    help='random seed (default: 543)')
-parser.add_argument('--render', action='store_true',
-                    help='render the environment')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='interval between training status logs (default: 10)')
-args = parser.parse_args()
-
-
-env = gym.make('CartPole-v1')
-env.seed(args.seed)
-torch.manual_seed(args.seed)
-
-
-class Policy(nn.Module):
-    def __init__(self):
-        super(Policy, self).__init__()
-        self.affine1 = nn.Linear(4, 128)
-        self.dropout = nn.Dropout(p=0.6)
-        self.affine2 = nn.Linear(128, 2)
-
-        self.saved_log_probs = []
-        self.rewards = []
+class PolicyNet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(PolicyNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
 
     def forward(self, x):
-        x = self.affine1(x)
-        x = self.dropout(x)
-        x = F.relu(x)
-        action_scores = self.affine2(x)
-        return F.softmax(action_scores, dim=1)
+        x = F.relu(self.fc1(x))
+        return F.softmax(self.fc2(x), dim = 1) # softmax() along the rows
+
+class REINFORCE:
+    def __init__(self, state_dim, hidden_dim, action_dim, lr, gamma, device):
+        self.policy_net = PolicyNet(state_dim, hidden_dim, action_dim).to(device)
+        self.optimizer = torch.optim.Adam(self.policy_net.parameters(), lr) # adam optimizer
+        self.gamma = gamma  # discount factor
+        self.device = device
+
+    def take_action(self, state): # random sampling according to action probability distribution
+        state = torch.tensor(state, dtype=torch.float).to(self.device)
+        state = torch.unsqueeze(state, 0)
+        probs = self.policy_net(state)
+        action_dist = torch.distributions.Categorical(probs)
+        action = action_dist.sample()
+        return action.item()
+
+    def update(self, transition_dict):
+        reward_list = transition_dict['rewards']
+        state_list = transition_dict['states']
+        action_list = transition_dict['actions']
+
+        G = 0
+        self.optimizer.zero_grad()
+        for i in reversed(range(len(reward_list))):
+            reward = reward_list[i]
+            state = torch.tensor(state_list[i]).float().to(self.device)
+            state = torch.unsqueeze(state, 0)
+            action = torch.tensor(action_list[i]).view(-1, 1).to(self.device)
+            log_prob = torch.log(self.policy_net(state).gather(1, action))
+            G =self.gamma * G + reward
+            loss = -log_prob * G
+            loss.backward()
+        self.optimizer.step()
 
 
-policy = Policy()
-optimizer = optim.Adam(policy.parameters(), lr=1e-2)
-eps = np.finfo(np.float32).eps.item()
+lr=1e-3
+num_episodes=1000
+hidden_dim=128
+gamma=0.98
 
+env_name = 'CartPole-v1'
+env = gym.make(env_name)
+env.reset(seed=0)
+torch.manual_seed(0)
 
-def select_action(state):
-    state = torch.from_numpy(state).float().unsqueeze(0)
-    probs = policy(state)
-    m = Categorical(probs)
-    action = m.sample()
-    policy.saved_log_probs.append(m.log_prob(action))
-    return action.item()
+print("State space: ", env.observation_space)
+print("State shape: ", env.observation_space.shape)
+print("- low: ", env.observation_space.low)
+print("- high: ", env.observation_space.high)
+print("State space samples: ", np.array([env.observation_space.sample() for i in range(5)]))
+print("\n")
+print("Action space:", env.action_space)
+print("Action shape:", env.action_space.shape)
+print("Action space samples:", np.array([env.action_space.sample() for i in range(5)]))
 
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.n
+agent = REINFORCE(state_dim, hidden_dim, action_dim, lr, gamma, device)
 
-def finish_episode():
-    R = 0
-    policy_loss = []
-    returns = []
-    for r in policy.rewards[::-1]:
-        R = r + args.gamma * R
-        returns.insert(0, R)
-    returns = torch.tensor(returns)
-    returns = (returns - returns.mean()) / (returns.std() + eps)
-    for log_prob, R in zip(policy.saved_log_probs, returns):
-        policy_loss.append(-log_prob * R)
-    optimizer.zero_grad()
-    policy_loss = torch.cat(policy_loss).sum()
-    policy_loss.backward()
-    optimizer.step()
-    del policy.rewards[:]
-    del policy.saved_log_probs[:]
+return_list = []
+for i in range(10):
+    with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+        for i_episode in range(int(num_episodes / 10)):
+            episode_return = 0
+            transition_dict = {'states': [], 'actions': [], 'next_states': [], 'rewards': [], 'dones': []}
+            state, info = env.reset()
+            episode_over = False
+            while not episode_over:
+                action = agent.take_action(state)
+                next_state, reward, done, truncated, info = env.step(action)
+                transition_dict['states'].append(state)
+                transition_dict['actions'].append(action)
+                transition_dict['next_states'].append(next_state)
+                transition_dict['rewards'].append(reward)
+                transition_dict['dones'].append(done)
+                episode_over = done or truncated
+                state = next_state
+                episode_return += reward
+            return_list.append(episode_return)
+            agent.update(transition_dict)
+            if (i_episode + 1) % 10 == 0:
+                pbar.set_postfix({'episode': '%d' % (num_episodes / 10 * i + i_episode + 1),
+                                  'return': '%.3f' % np.mean(return_list[-10:])})
+            pbar.update(1)
 
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('REINFORCE on {}'.format(env_name))
+plt.show()
 
-def main():
-    running_reward = 10
-    for i_episode in count(1):
-        state, ep_reward = env.reset(), 0
-        for t in range(1, 10000):  # Don't infinite loop while learning
-            action = select_action(state)
-            state, reward, done, _ = env.step(action)
-            if args.render:
-                env.render()
-            policy.rewards.append(reward)
-            ep_reward += reward
-            if done:
-                break
-
-        running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
-        finish_episode()
-        if i_episode % args.log_interval == 0:
-            print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
-                  i_episode, ep_reward, running_reward))
-        if running_reward > env.spec.reward_threshold:
-            print("Solved! Running reward is now {} and "
-                  "the last episode runs to {} time steps!".format(running_reward, t))
-            break
-
-
-if __name__ == '__main__':
-    main()
+mv_return = rl_utils.moving_average(return_list, 9)
+plt.plot(episodes_list, mv_return)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('REINFORCE on {}'.format(env_name))
+plt.show()
