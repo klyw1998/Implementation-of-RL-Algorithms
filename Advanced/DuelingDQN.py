619d9a9d751bb31f83c51f31dc699e627f00c6a2
Liangyawei Kuang <33073967+klyw1998@users.noreply.github.com>
2025-04-30 04:01:57 +0800

    Add files via upload

---
+++ Advanced/DuelingDQN.py
@@ -0,0 +1,171 @@
+import numpy as np
+import random
+import gymnasium as gym
+import torch
+import torch.nn.functional as F
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import rl_utils
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print(device)
+
+class Qnet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(Qnet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+
+class VAnet(torch.nn.Module):
+    "V net and A net with one hidden layer"
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(VAnet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc_A = torch.nn.Linear(hidden_dim, action_dim)
+        self.fc_V = torch.nn.Linear(hidden_dim, 1)
+
+    def forward(self, x):
+        A = self.fc_A(F.relu(self.fc1(x)))
+        V = self.fc_V(F.relu(self.fc1(x)))
+        Q = V + A - A.mean(-1).view(-1, 1)
+        return Q
+
+class DQN:
+    "DQN algorithm, including double DQN"
+    def __init__(self, state_dim, hidden_dim, action_dim, lr, gamma, epsilon, target_update, device, dqn_type):
+        self.action_dim = action_dim
+        if dqn_type == "DuelingDQN":
+            self.q_net = VAnet(state_dim, hidden_dim, self.action_dim).to(device)
+            self.target_q_net = VAnet(state_dim, hidden_dim, self.action_dim).to(device)
+        else:
+            self.q_net = Qnet(state_dim, hidden_dim, self.action_dim).to(device)
+            self.target_q_net = Qnet(state_dim, hidden_dim, self.action_dim).to(device)
+        self.optimizer = torch.optim.Adam(self.q_net.parameters(), lr) # use adam optimizer
+        self.gamma = gamma # discount factor
+        self.epsilon = epsilon # epsilon-greedy policy
+        self.target_update = target_update # target network update frequence
+        self.count = 0
+        self.device = device
+        self.dqn_type = dqn_type
+
+    def take_action(self, state): # take action following epsilon-greedy
+        if np.random.random() < self.epsilon:
+            action = np.random.randint(self.action_dim)
+        else:
+            state = np.array(state)
+            state = torch.tensor(state, dtype=torch.float).to(self.device)
+            action = self.q_net(state).argmax().item()
+        return action
+
+    def max_q_value(self, state):
+        state = np.array(state)
+        state = torch.tensor(state, dtype=torch.float).to(self.device)
+        return  self.q_net(state).max().item()
+
+    def update(self, transition_dict):
+        states = torch.tensor(transition_dict['states']).to(self.device)
+        actions = torch.tensor(transition_dict['actions']).view(-1, 1).to(self.device)
+        rewards = torch.tensor(transition_dict['rewards']).float().view(-1, 1).to(self.device)
+        next_states = torch.tensor(np.array(transition_dict['next_states'])).to(self.device)
+        dones = torch.tensor(transition_dict['dones']).float().view(-1, 1).to(self.device)
+        q_values = self.q_net(states).gather(1, actions) # Q_value
+        if self.dqn_type == "DoubleDQN": # DoubleDQN
+            max_actions = self.q_net(next_states).max(1)[1].view(-1, 1)
+            max_next_q_values = self.target_q_net(next_states).gather(1, max_actions)
+        else: # DQN
+            max_next_q_values = self.target_q_net(next_states).max(1)[0].view(-1, 1)
+        q_targets = rewards + self.gamma * max_next_q_values * (1 - dones) # TD loss target
+        dqn_loss = torch.mean(F.mse_loss(q_values, q_targets)) # mean squared error loss function
+        self.optimizer.zero_grad() # the default gradient will be accumulated in PyTorch so we need to set it to zero
+        dqn_loss.backward() # update parameters backwards
+        self.optimizer.step()
+        if self.count % self.target_update == 0:
+            self.target_q_net.load_state_dict(self.q_net.state_dict()) # update target net
+        self.count +=1
+
+
+lr = 1e-2
+num_episodes = 200
+hidden_dim = 128
+gamma = 0.98
+epsilon = 0.01
+target_update = 50
+buffer_size = 5000
+minimal_size = 1000
+batch_size = 64
+
+env_name = 'Pendulum-v1'
+env = gym.make(env_name)
+state_dim = env.observation_space.shape[0]
+action_dim = 11 # separate continuous action to discrete actions
+
+def dis_to_con(discrete_action, env, action_dim):
+    action_lowbound = env.action_space.low[0]
+    action_upbound = env.action_space.high[0]
+    return action_lowbound + (discrete_action / (action_dim - 1)) * (action_upbound - action_lowbound)
+
+def train_DQN(agent, env, num_episodes, replay_buffer, minimal_size, batch_size):
+    return_list = []
+    max_q_value_list = []
+    max_q_value = 0
+    for i in range(10):
+        with tqdm(total=int(num_episodes / 10), desc='Iteration %d' % i) as pbar:
+            for i_episode in range(int(num_episodes / 10)):
+                episode_return = 0
+                state, info = env.reset()
+                episode_over = False
+                while not episode_over:
+                    action = agent.take_action(state)
+                    max_q_value = agent.max_q_value(state) * 0.005 + max_q_value * 0.995 # smoothing
+                    max_q_value_list.append(max_q_value)  # save max Q for each state
+                    action_continuous = dis_to_con(action, env, agent.action_dim)
+                    next_state, reward, done, truncated, info = env.step([action_continuous])
+                    replay_buffer.add(state, action, reward, next_state, done)
+                    episode_over = done or truncated
+                    episode_return += reward
+                    state = next_state
+                    if replay_buffer.size() > minimal_size:
+                        b_s, b_a, b_r, b_ns, b_d = replay_buffer.sample(batch_size)
+                        transition_dict = {'states': b_s, 'actions': b_a, 'next_states': b_ns, 'rewards': b_r, 'dones': b_d}
+                        agent.update(transition_dict)
+                return_list.append(episode_return)
+                if (i_episode + 1) % 10 == 0:
+                    pbar.set_postfix({'episode': '%d' % (num_episodes / 10 * i + i_episode + 1),
+                                      'return': '%.3f' % np.mean(return_list[-10:])})
+                pbar.update(1)
+    return return_list, max_q_value_list
+
+random.seed(0)
+np.random.seed(0)
+env.reset(seed=0)
+torch.manual_seed(0)
+replay_buffer = rl_utils.ReplayBuffer(buffer_size)
+# agent = DQN(state_dim, hidden_dim, action_dim, lr, gamma, epsilon, target_update, device, "VanillaDQN")
+# agent = DQN(state_dim, hidden_dim, action_dim, lr, gamma, epsilon, target_update, device, "DoubleDQN")
+agent = DQN(state_dim, hidden_dim, action_dim, lr, gamma, epsilon, target_update, device, "DuelingDQN")
+return_list, max_q_value_list = train_DQN(agent, env, num_episodes, replay_buffer, minimal_size, batch_size)
+
+episodes_list = list(range(len(return_list)))
+mv_return = rl_utils.moving_average(return_list, 5)
+plt.plot(episodes_list, mv_return)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+# plt.title('DQN on {}'.format(env_name))
+# plt.title('DoubleDQN on {}'.format(env_name))
+plt.title('DuelingDQN on {}'.format(env_name))
+plt.show()
+
+frames_list = list(range(len(max_q_value_list)))
+plt.plot(frames_list, max_q_value_list)
+plt.axhline(0, c='orange', ls='--')
+plt.axhline(10, c='red', ls='--')
+plt.xlabel('Frames')
+plt.ylabel('Q value')
+# plt.title('DQN on {}'.format(env_name))
+# plt.title('DoubleDQN on {}'.format(env_name))
+plt.title('DuelingDQN on {}'.format(env_name))
+plt.show()
---
+++ Advanced/TRPO.py
@@ -0,0 +1,185 @@
+import numpy as np
+import gymnasium as gym
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import rl_utils
+import copy
+
+# Check whether GPU is available
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+print(device)
+
+def compute_advantage(gamma, lmbda, td_delta):
+    # generalized advantage estimation
+    td_delta = td_delta.detach().numpy()
+    advantage_list = []
+    advantage = 0.0
+    for delta in td_delta[::-1]:
+        advantage = gamma * lmbda * advantage + delta
+        advantage_list.append(advantage)
+    advantage_list.reverse()
+    return torch.tensor(np.array(advantage_list), dtype=torch.float)
+
+class PolicyNet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(PolicyNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return F.softmax(self.fc2(x), dim = 1)
+
+class ValueNet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(ValueNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+
+class TRPO:
+    "TRPO algo."
+    def __init__(self, hidden_dim, state_space, action_space, lmbda, kl_constraint, alpha, critic_lr, gamma, device):
+        state_dim = state_space.shape[0]
+        action_dim = action_space.n
+        # parameters in policy net do not need a optimizer to update
+        self.actor = PolicyNet(state_dim, hidden_dim, action_dim).to(device)
+        self.critic = ValueNet(state_dim, hidden_dim, 1).to(device)
+        self.critic_optimizer = torch.optim.Adam(self.critic.parameters(), lr=critic_lr)
+        self.gamma = gamma
+        self.lmbda = lmbda # GAE parameter
+        self.kl_constraint = kl_constraint # max limit for KL distance
+        self.alpha = alpha # linear research parameter
+        self.device = device
+
+    def take_action(self, state):
+        state = torch.unsqueeze(torch.tensor(state, dtype=torch.float).to(self.device), 0)
+        probs = self.actor(state)
+        action_dist = torch.distributions.Categorical(probs)
+        action = action_dist.sample()
+        return action.item()
+
+    def hessian_matrix_vector_product(self, states, action_dists_old, vector):
+        # calculate the product of the hessian matrix and a vector
+        action_dists_new = torch.distributions.Categorical(self.actor(states))
+        kl = torch.mean(torch.distributions.kl.kl_divergence(action_dists_old, action_dists_new)) # calculate the mean kl distance
+        kl_grad = torch.autograd.grad(kl, self.actor.parameters(),  create_graph=True)
+        kl_grad_vector = torch.cat([grad.view(-1) for grad in kl_grad])
+        # calculate the product of the gradient vector of kl distance and the vector
+        kl_grad_vector_product = torch.dot(kl_grad_vector, vector)
+        grad2 = torch.autograd.grad(kl_grad_vector_product, self.actor.parameters())
+        grad2_vector = torch.cat([grad.view(-1) for grad in grad2])
+        return grad2_vector
+
+    def conjugate_gradient(self, grad, states, old_action_dists):
+        # conjugate gradient method
+        x = torch.zeros_like(grad)
+        r = grad.clone()
+        p = grad.clone()
+        rdr = torch.dot(r, r)
+        for i in range(10):
+            Hp = self.hessian_matrix_vector_product(states, old_action_dists, p)
+            alpha = rdr / torch.dot(p, Hp)
+            x += alpha * p
+            r -= alpha * Hp
+            rdr_new = torch.dot(r, r)
+            if rdr_new < 1e-10:
+                break
+            beta = rdr_new / rdr
+            p = r + beta * p
+            rdr = rdr_new
+        return x
+
+    def compute_surrogate_obj(self, states, actions, advantage, log_probs_old, actor):
+        # calculate the policy target
+        log_probs = torch.log(actor(states).gather(1, actions))
+        ratio = torch.exp(log_probs - log_probs_old)
+        return torch.mean(ratio * advantage)
+
+    def line_search(self, states, actions, advantage, log_probs_old, action_dists_old, max_vec):
+        # line search
+        para_old= torch.nn.utils.convert_parameters.parameters_to_vector(self.actor.parameters())
+        obj_old = self.compute_surrogate_obj(states, actions, advantage, log_probs_old, self.actor)
+        for i in range(15):
+            coef = self.alpha ** i
+            para_new = para_old + coef * max_vec
+            actor_new = copy.deepcopy(self.actor)
+            torch.nn.utils.convert_parameters.vector_to_parameters(para_new, actor_new.parameters())
+            action_dists_new = torch.distributions.Categorical(actor_new(states))
+            kl_div = torch.mean(torch.distributions.kl.kl_divergence(action_dists_old,action_dists_new))
+            obj_new = self.compute_surrogate_obj(states, actions, advantage, log_probs_old, actor_new)
+            if obj_new > obj_old and kl_div < self.kl_constraint:
+                return para_new
+        return para_old
+    def policy_learn(self, states, actions, action_dists_old, log_probs_old, advantage):
+        # update policy parameters
+        surrogate_obj = self.compute_surrogate_obj(states, actions, advantage, log_probs_old, self.actor)
+        grads = torch.autograd.grad(surrogate_obj, self.actor.parameters())
+        obj_grad = torch.cat([grad.view(-1) for grad in grads]).detach()
+        # calculate x = H^(-1)g
+        descent_direction = self.conjugate_gradient(obj_grad, states, action_dists_old)
+        Hd = self.hessian_matrix_vector_product(states, action_dists_old, descent_direction)
+        coef_max = torch.sqrt(2 * self.kl_constraint / (torch.dot(descent_direction, Hd) + 1e-8))
+        para_new = self.line_search(states, actions, advantage, log_probs_old, action_dists_old, descent_direction * coef_max)  # linear search
+        torch.nn.utils.convert_parameters.vector_to_parameters(para_new, self.actor.parameters())
+
+    def update(self, transition_dict):
+        states = torch.tensor(np.array(transition_dict['states'])).float().to(self.device)
+        actions = torch.tensor(transition_dict['actions']).view(-1, 1).to(self.device)
+        rewards = torch.tensor(transition_dict['rewards']).view(-1, 1).to(self.device)
+        next_states = torch.tensor(np.array(transition_dict['next_states'])).to(self.device)
+        dones = torch.tensor(transition_dict['dones']).float().view(-1, 1).to(self.device)
+        td_target = rewards + self.gamma * self.critic(next_states) * (1 - dones)
+        td_delta = td_target - self.critic(states)
+        advantage = compute_advantage(self.gamma, self.lmbda, td_delta.cpu()).to(self.device) # maintain the critic in cpu but not cuda
+        log_probs_old = torch.log(self.actor(states).gather(1, actions)).detach()
+        action_dists_old = torch.distributions.Categorical(self.actor(states).detach())
+        critic_loss = torch.mean(F.mse_loss(self.critic(states), td_target.detach()))
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+        self.policy_learn(states, actions, action_dists_old, log_probs_old, advantage)
+
+num_episodes = 1000
+hidden_dim = 128
+gamma = 0.98
+lmbda = 0.95
+critic_lr = 1e-2
+kl_constraint = 5e-4
+alpha = 0.5
+
+env_name = 'CartPole-v1'
+env = gym.make(env_name)
+env.reset(seed=0)
+torch.manual_seed(0)
+
+print("State space: ", env.observation_space)
+print("State shape: ", env.observation_space.shape)
+print("- low: ", env.observation_space.low)
+print("- high: ", env.observation_space.high)
+print("State space samples: ", np.array([env.observation_space.sample() for i in range(5)]))
+print("\n")
+print("Action space:", env.action_space)
+print("Action shape:", env.action_space.shape)
+print("Action space samples:", np.array([env.action_space.sample() for i in range(5)]))
+
+agent = TRPO(hidden_dim, env.observation_space, env.action_space, lmbda, kl_constraint, alpha, critic_lr, gamma, device)
+return_list = rl_utils.train_on_policy_agent(env, agent, num_episodes)
+
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('TRPO on {}'.format(env_name))
+plt.show()
+
+mv_return = rl_utils.moving_average(return_list, 9)
+plt.plot(episodes_list, mv_return)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('TRPO on {}'.format(env_name))
+plt.show()
